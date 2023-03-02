801b7c28f0d687060957621dbeb90ea78152c54a
Otabek <otabekjon2002@mail.ru>
2023-03-03 02:39:25 +0500

    How to add or remove a product from user wishlist

--- backend/controller/productCtrl.js
+++ backend/controller/productCtrl.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productModel')
+const User = require('../models/userModel')
 //
 const slugify = require('slugify')
 const { query } = require('express')
@@ -21,43 +22,42 @@ const createProduct = asyncHandler(async (req, res) => {
 const getAllProducts = asyncHandler(async (req, res) => {
   try {
     // filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach((el) => delete queryObj[el]);
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    const queryObj = { ...req.query }
+    const excludeFields = ['page', 'sort', 'limit', 'fields']
+    excludeFields.forEach(el => delete queryObj[el])
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
-    let query = Product.find(JSON.parse(queryStr));
-
+    let query = Product.find(JSON.parse(queryStr))
 
     // sorting the data
-    if(req.query.sort){
-        const sortBy = req.query.sort.split(",").join(" ");
-        query = query.sort(sortBy)
-    }else{
-        query = query.sort('-createdAt')
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
     }
 
     // limiting the fields
     if (req.query.fields) {
-        const fields = req.query.fields.split(",").join(" ");
-        query = query.select(fields);
-      } else {
-        query = query.select("-__v");
-      }
-
-    // pagination
-    const page  = req.query.page;
-    const limit = req.query.limit;
-    const skip = req.query.slip;
-    query = query.skip(skip).limit(limit);
-    if (req.query.page) {
-        const productCount = await Product.countDocuments();
-        if(skip >= productCount) throw new Error('This page is not excist')
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields)
+    } else {
+      query = query.select('-__v')
     }
 
-      const product = await query;
-      res.json(product);
+    // pagination
+    const page = req.query.page
+    const limit = req.query.limit
+    const skip = req.query.slip
+    query = query.skip(skip).limit(limit)
+    if (req.query.page) {
+      const productCount = await Product.countDocuments()
+      if (skip >= productCount) throw new Error('This page is not excist')
+    }
+
+    const product = await query
+    res.json(product)
   } catch (error) {
     throw new Error(error)
   }
@@ -101,10 +101,45 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 })
 
+// add product to user wishlist
+const addToWishList = asyncHandler(async (req, res) => {
+  const { _id } = req.user
+  const { productId } = req.body
+  try {
+    const user = await User.findById(_id)
+    const alreadyAdded = user.wishlist.find((id) => id.toString() === productId)
+    if (alreadyAdded) {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $pull: { wishlist: productId }
+        },
+        {
+          new: true
+        }
+      )
+      res.json(user)
+    } else {
+      let user = await User.findByIdAndUpdate(
+        _id,
+        {
+          $push: { wishlist: productId }
+        },
+        {
+          new: true
+        }
+      )
+      res.json(user)
+    }
+  } catch (error) {
+    throw new Error(error)
+  }
+})
 module.exports = {
   createProduct,
   getSingleProduct,
   getAllProducts,
   updateProduct,
-  deleteProduct
+  deleteProduct,
+  addToWishList
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {isAuthenticated, isAdmin } = require('../middleware/auth');
-const { createProduct, getSingleProduct, getAllProducts, updateProduct, deleteProduct } = require('../controller/productCtrl');
+const { createProduct, getSingleProduct, getAllProducts, updateProduct, deleteProduct, addToWishList } = require('../controller/productCtrl');
 
 // Create Product  ----- Admin
 router.post('/',isAuthenticated, isAdmin, createProduct);
 
 //Get All Products
 router.get('/', getAllProducts)
+
+//Add Product to User wishlist 
+router.put('/wishlist', isAuthenticated, addToWishList)
 
 //Get Single Product  ----- Admin
 router.get('/:id',isAuthenticated, isAdmin, getSingleProduct)
@@ -17,6 +20,7 @@ router.put('/:id',isAuthenticated, isAdmin, updateProduct)
 
 //Delete Product  ----- Admin
 router.delete('/:id',isAuthenticated, isAdmin, deleteProduct)
+
 
 
 module.exports = router;
