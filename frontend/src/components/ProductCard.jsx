453ae370e894317911bab529dcaa00a83a2d7523
Otabek <otabekjon2002@mail.ru>
2023-03-13 01:30:15 +0500

    Created : OurStore page

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const { grid} = props;
+  console.log(grid);
+  let location = useLocation();
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
+    <>
+    <div
+      className={` ${
+        location.pathname === "/store" ? `gr-${grid}` : "col-3"
+      } `}
+    >
+      <Link 
+      to={`${
+        location.pathname === "/"
+          ? "/product/:id"
+          : location.pathname === "/product/:id"
+          ? "/product/:id"
+          : ":id"
+      }`}
+      className="product-card position-relative">
+        
         <div className="wishlist-icon position-absolute">
           <Link>
             <img src="./images/wish.svg" alt="Wishlist" />
           </Link>
         </div>
         <div className="product-img">
-          <img  className="img-fluid" src="./images/watch-2.jpg" alt="Product Img" />
-          <img  className="img-fluid" src="./images/watch.jpg" alt="Product Img" />
+          <img
+            className="img-fluid"
+            src="./images/watch-2.jpg"
+            alt="Product Img"
+          />
+          <img
+            className="img-fluid"
+            src="./images/watch.jpg"
+            alt="Product Img"
+          />
         </div>
         <div className="product-details">
           <h5 className="brand">Apple Watch</h5>
@@ -24,10 +49,16 @@ const ProductCard = () => {
             count={5}
             // onChange={ratingChanged}
             size={24}
-            value="3"
+            value={3}
             edit={false}
             activeColor="#ffd700"
           />
+          <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt...
+            </p>
           <p className="price">$100.00</p>
         </div>
         <div className="action-bar position-absolute">
@@ -45,6 +76,7 @@ const ProductCard = () => {
         </div>
       </Link>
     </div>
+    </>
   );
 };
 
